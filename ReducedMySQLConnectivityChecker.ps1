ce99a2ae89aa5554aa0310e9ad64c3b8c89bb20d
Shawn Xiao <xixia@microsoft.com>
2022-04-11 20:58:04 +0800

    update mysql connector and change string

--- ReducedMySQLConnectivityChecker.ps1
+++ ReducedMySQLConnectivityChecker.ps1
@@ -260,7 +260,7 @@ try {
         Remove-Item $path
     }
     
-    Invoke-WebRequest -Uri $('https://github.com/ShawnXxy/SQL-Connectivity-Checker/raw/' + $RepositoryBranch + '/netstandard2.0/TDSClient.dll') -OutFile $path -UseBasicParsing
+    Invoke-WebRequest -Uri $('https://github.com/ShawnXxy/SQL-Connectivity-Checker/tree/xixia/raw/' + $RepositoryBranch + '/netstandard2.0/TDSClient.dll') -OutFile $path -UseBasicParsing
     
     $path = $env:TEMP + "/TDSClient.dll"
     $assembly = [System.IO.File]::ReadAllBytes($path)
