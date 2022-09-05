2f68a1bcae1060f0a73713fc6df6fdf8c45128bd
marlonj-ms <marlonj@microsoft.com>
2022-09-05 21:37:37 +0800

    Update AzureMySQLConnectivityChecker.ps1

--- AzureMySQLConnectivityChecker.ps1
+++ AzureMySQLConnectivityChecker.ps1
@@ -1384,11 +1384,11 @@ try {
         $MySQLDllPath = Join-Path ((Get-Location).Path) "MySql.Data.dll"
         if ($Local) {
             Copy-Item -Path $($LocalPath + '/netstandard2.0/TDSClient.dll') -Destination $MySQLDllPath
+            Write-Host 'Local'+$Local
+            Write-Host 'LocalPath'+$LocalPath
+            Write-Host 'MySQLDllPath'+$MySQLDllPath
         }
-        Write-Host 'Local'+$Local
-        Write-Host 'LocalPath'+$LocalPath
-        Write-Host 'MySQLDllPath'+$MySQLDllPath
-        else {
+         else {
             Invoke-WebRequest -Uri $('https://github.com/Azure/SQL-Connectivity-Checker/raw/' + $RepositoryBranch + '/netstandard2.0/TDSClient.dll') -OutFile $TDSClientPath -UseBasicParsing
         }
         #$assembly = [System.IO.File]::ReadAllBytes($TDSClientPath)
