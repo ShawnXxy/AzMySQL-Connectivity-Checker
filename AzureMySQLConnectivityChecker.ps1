8109436b6565ea2c1b6722cb3044f6aa0c62fafb
marlonj-ms <marlonj@microsoft.com>
2022-09-20 15:39:05 +0800

    Update AzureMySQLConnectivityChecker.ps1

--- AzureMySQLConnectivityChecker.ps1
+++ AzureMySQLConnectivityChecker.ps1
@@ -652,7 +652,7 @@ function TestConnectionToDatabase($Server, $gatewayPort, $Database, $User, $Pass
  #           [void]$summaryRecommendedAction.AppendLine('It seems that the user/password is not correct. Please verify if the correct username/password is placed for a sucessful authentitication.')
 #            [void]$summaryRecommendedAction.AppendLine('If you are trying to make connections via an AAD account, please configure the AAD setting in Portal first. Ref: https://docs.microsoft.com/en-us/azure/mysql/single-server/how-to-configure-sign-in-azure-ad-authentication')
            [void]$summaryLog.AppendLine($MySQL_AccessDeniedError )
-         [void]$summaryRecommendedAction.AppendLine()
+
           [void]$summaryRecommendedAction.AppendLine($MySQL_AccessDeniedErrorAction)
 
             TrackWarningAnonymously ('TestConnectionToDatabase | 1045: ' + $erMsg)
