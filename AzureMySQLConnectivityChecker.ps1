b7757d363fb704523b04b445f67ffd7561d90f11
marlonj-ms <marlonj@microsoft.com>
2022-09-20 15:41:58 +0800

    Update AzureMySQLConnectivityChecker.ps1

--- AzureMySQLConnectivityChecker.ps1
+++ AzureMySQLConnectivityChecker.ps1
@@ -181,11 +181,6 @@ Having DNS resolution switching between a couple of Gateway addresses is expecte
 If you are using Private Link, a mismatch between your DNS server and OpenDNS is expected.
 Please review the DNS results.'
 
-$MySQL_AccessDeniedError = 'Connection to database ' + $Database + ' failed because the username/password is wrong.'
-$MySQL_AccessDeniedErrorAction ='It seems that the user/password is not correct. Please verify if the correct username/password is placed for a sucessful authentitication.
-If you are trying to make connections via an AAD account, please configure the AAD setting in Portal first. Ref: https://docs.microsoft.com/en-us/azure/mysql/single-server/how-to-configure-sign-in-azure-ad-authentication'
-
-
 # $DNSResolutionFailedAzureMySQLFlexPublic = ' Please make sure the server name FQDN is correct and that your machine can resolve it.
 #  If public endpoint is enabled, failure to resolve domain name for your logical server is almost always the result of specifying an invalid/misspelled server name,
 #  or a client-side networking issue that you will need to pursue with your local network administrator.'
@@ -575,8 +570,7 @@ function TestConnectionToDatabase($Server, $gatewayPort, $Database, $User, $Pass
 
         return $true
 
-    } catch #[MySql.Data.MySqlClient.MySqlException] 
-    {
+    } catch [MySql.Data.MySqlClient.MySqlException] {
         $erno = $_.Exception.Number
         $erMsg = $_.Exception.Message
         Write-Host ([string]::Format("The connection to server {0} and database {1} Failed because of the error below.", $Server,$Database)) -ForegroundColor Red
@@ -649,16 +643,13 @@ function TestConnectionToDatabase($Server, $gatewayPort, $Database, $User, $Pass
             Write-Host 'Error Message:' 
             Write-Host ' ' $erMsg #-ForegroundColor Yellow
     
-            #$msg = 'Connection to database ' + $Database + ' failed due to that the username/password is wrong.'
+            $msg = 'Connection to database ' + $Database + ' failed due to that the username/password is wrong.'
     
-  #          [void]$summaryLog.AppendLine($msg)
-  #          [void]$summaryRecommendedAction.AppendLine()
- #           [void]$summaryRecommendedAction.AppendLine($msg)
- #           [void]$summaryRecommendedAction.AppendLine('It seems that the user/password is not correct. Please verify if the correct username/password is placed for a sucessful authentitication.')
-#            [void]$summaryRecommendedAction.AppendLine('If you are trying to make connections via an AAD account, please configure the AAD setting in Portal first. Ref: https://docs.microsoft.com/en-us/azure/mysql/single-server/how-to-configure-sign-in-azure-ad-authentication')
-           [void]$summaryLog.AppendLine($MySQL_AccessDeniedError )
-
-          [void]$summaryRecommendedAction.AppendLine($MySQL_AccessDeniedErrorAction)
+            [void]$summaryLog.AppendLine($msg)
+            [void]$summaryRecommendedAction.AppendLine()
+            [void]$summaryRecommendedAction.AppendLine($msg)
+            [void]$summaryRecommendedAction.AppendLine('It seems that the user/password is not correct. Please verify if the correct username/password is placed for a sucessful authentitication.')
+            [void]$summaryRecommendedAction.AppendLine('If you are trying to make connections via an AAD account, please configure the AAD setting in Portal first. Ref: https://docs.microsoft.com/en-us/azure/mysql/single-server/how-to-configure-sign-in-azure-ad-authentication')
 
             TrackWarningAnonymously ('TestConnectionToDatabase | 1045: ' + $erMsg)
             return $false
